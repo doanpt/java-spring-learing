bd7cd01c87bc9b141323b617cba01d60862d3bb3
Trung Doan <doanpt93cntt@gmail.com>
2021-06-22 11:42:54 +0700

    file upload project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+//    https://www.mkyong.com/spring-boot/spring-rest-hello-world-example/
+//    https://www.mkyong.com/spring-boot/spring-boot-file-upload-example-ajax-and-rest/
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
